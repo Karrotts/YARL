71562f5b56e1a1bc0e02677ec8ee7e79f46aba0f
Karrotts <wesleymiller1996@gmail.com>
2023-11-13 21:29:36 -0500

    added speed modifier to character

--- Scripts/EntityBase/Character.cs
+++ Scripts/EntityBase/Character.cs
@@ -4,13 +4,35 @@ using System;
 public partial class Character : CharacterBody2D
 {
     [Export]
+    [ExportGroup("Nodes")]
     public HealthComponent HealthComponent { get; set; }
 
     [Export]
+    [ExportGroup("Nodes")]
     public MovementComponent MovementComponent { get; set; }
+
+    [Export]
+    [ExportGroup("Modifiers")]
+    public int SpeedModifier
+    {
+        get { return _speedModifier; }
+        set 
+        {
+            if (MovementComponent != null)
+            {
+                MovementComponent.CurrentSpeed += value - _speedModifier;
+            }
+            _speedModifier = value;
+        }
+    }
+
+    private int _speedModifier;
 
     public override void _Ready()
     {
+        // reset values to trigger update methods
+        SpeedModifier = _speedModifier * -1;
+        SpeedModifier = Mathf.Abs(_speedModifier);
     }
 
     public override void _Process(double delta)
